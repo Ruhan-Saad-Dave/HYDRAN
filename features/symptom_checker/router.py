a755b174fe81ff89d9602404346bac5680d4f640
Hano Varghese <hanojoby@gmail.com>
2025-09-23 14:20:43 +0530

    Chore: Fixed temporary redirect issue

--- features/symptom_checker/router.py
+++ features/symptom_checker/router.py
@@ -6,7 +6,8 @@ symptom_router = APIRouter(
     tags=["symptom_checker"], 
 )
 
-@symptom_router.post("/", response_model=QueryResponse)
+@symptom_router.post("", response_model=QueryResponse)
+@symptom_router.post("/", response_model=QueryResponse, include_in_schema=False)
 async def create_query(query: UserQuery):
     response = get_symptom_checker_response(query.session_id, query.query)
     return QueryResponse(response=response)
